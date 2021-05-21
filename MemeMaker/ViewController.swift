8f235fee5f5d7faf5dc23114defb6b6b95ab97d2
Swift-Teacher <brian@swiftteacher.org>
2021-05-21 14:18:49 -0400

    Ability to add the users photo has been added.

--- MemeMaker/ViewController.swift
+++ MemeMaker/ViewController.swift
@@ -1,24 +1,49 @@
+//
+//  ViewController.swift
+//  MemeMaker
+//
+//  Created by Brian Foutty on 3/17/21.
+//  This is the concept app version.
+
 import UIKit
+import CoreImage
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    // MARK: - IB Outlets
     @IBOutlet weak var topSegmentedControl: UISegmentedControl!
     @IBOutlet weak var bottomSegmentedControl: UISegmentedControl!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
     
     @IBOutlet weak var topCaptionLabel: UILabel!
     @IBOutlet weak var bottomCaptionLabel: UILabel!
     
+    // MARK: - Instance Properties
+    // #26a
     let topChoices = [
-        CaptionOption(emoji: "🕶", caption: "You know what's cool?"),
-        CaptionOption(emoji: "💥", caption: "You know what makes me mad?"),
-        CaptionOption(emoji: "💕", caption: "You know what I love?")
+        CaptionOption(emoji: "😱", caption: "OMG!!"),
+        CaptionOption(emoji: "👀", caption: "Hey, look at this!"),
+        CaptionOption(emoji: "💕", caption: "You know what I love…"),
+        CaptionOption(emoji: "🤬", caption: "You know what makes me mad?"),
+        CaptionOption(emoji: "😳", caption: "Holy crap!!💩")
+    ]
+    // #26b
+    let bottomChoices = [
+        CaptionOption(emoji: "🤖", caption: "You are heartless."),
+        CaptionOption(emoji: "😂", caption: "Wow! That's funny!"),
+        CaptionOption(emoji: "👽", caption: "Dumb humans!"),
+        CaptionOption(emoji: "🤷", caption: "Beats me!?!?"),
+        CaptionOption(emoji: "👨‍💻", caption: "Not now. I'm in the zone.")
     ]
     
-    let bottomChoices = [
-        CaptionOption(emoji: "🐱", caption: "Cats wearing hats"),
-        CaptionOption(emoji: "🐕", caption: "Dogs carrying logs"),
-        CaptionOption(emoji: "🐒", caption: "Monkeys being funky")
-    ]
+    // #28
+    var currentImage: UIImage!
+    
+    // #29
+    var fontSize: Double = 70.0
+    var selectedFont = "Avenir Next Heavy"
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,13 +64,7 @@ class ViewController: UIViewController {
         updateCaptions()
     }
     
-    func updateCaptions() {
-        let topIndex = topSegmentedControl.selectedSegmentIndex
-        topCaptionLabel.text = topChoices[topIndex].caption
-        
-        let bottomIndex = bottomSegmentedControl.selectedSegmentIndex
-        bottomCaptionLabel.text = bottomChoices[bottomIndex].caption
-    }
+    // MARK: - IB Actions
 
     @IBAction func segmentedControlChanged(_ sender: Any) {
         updateCaptions()
@@ -62,5 +81,32 @@ class ViewController: UIViewController {
             bottomCaptionLabel.center = sender.location(in: view)
         }
     }
+    
+    @IBAction func importPicture(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+    
+    // MARK: - Instance Methods
+    func updateCaptions() {
+        let topIndex = topSegmentedControl.selectedSegmentIndex
+        topCaptionLabel.text = topChoices[topIndex].caption
+        
+        let bottomIndex = bottomSegmentedControl.selectedSegmentIndex
+        bottomCaptionLabel.text = bottomChoices[bottomIndex].caption
+    }
+    
+    // method to add selected photo from photo library to imageView
+    // #14
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        // #15
+        guard let image = info[.editedImage] as? UIImage else { return }
+        
+        imageView.image = image
+        dismiss(animated: true)
+    }
+    
 }
 
